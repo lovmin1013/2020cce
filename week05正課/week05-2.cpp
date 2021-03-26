4e41d428eabc9076c878a032568f8d3460d84919
lovmin1013 <79676872+lovmin1013@users.noreply.github.com>
2021-03-26 10:58:53 +0800

    Rename week05-3.cpp to week05-2.cpp

--- week05正課/week05-2.cpp
+++ week05正課/week05-2.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main()
-{///5�Ӧr��,�C�Ӧr�꦳10��
+{///5­Ó¦r¦ê,¨C­Ó¦r¦ê¦³10®æ
 
     char line[5][10]={"decline","proper","majority","bullet","shop"};
     for(int i=0; i<5; i++){
