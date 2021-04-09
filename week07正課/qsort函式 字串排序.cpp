065a3d5ae4959227203657d34d1ae3a5c8f45d59
lovmin1013 <79676872+lovmin1013@users.noreply.github.com>
2021-04-09 11:06:00 +0800

    Add files via upload

---
+++ week07正課/qsort函式 字串排序.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+char line[100][10];
+char temp[10];
+int compare (const void *p1, const void *p2)
+{
+    char *s1 = (char*) p1;
+    char *s2 = (char*) p2;
+    return strcmp(s1,s2);
+}
+int main()
+{
+	int N;
+	scanf("%d",&N);
+	for(int i=0; i<N; i++){
+		scanf("%s",line[i]);
+	}
+
+	qsort(line, N, 10, compare);
+
+	for(int i=0; i<N; i++){
+		printf("%s\n",line[i]);
+	}
+}
