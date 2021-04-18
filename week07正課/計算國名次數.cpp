4f8b42eda034498ffc5ed22d3999f6356c8221ac
lovmin1013 <79676872+lovmin1013@users.noreply.github.com>
2021-04-18 11:55:42 +0800

    Add files via upload

---
+++ week07正課/計算國名次數.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+char line[2000][75];
+char other[100];
+int compare(const void *p1, const void *p2)
+{
+	char *s1 = (char*) p1;
+	char *s2 = (char*) p2;
+	return strcmp(s1,s2);
+}
+int main()
+{
+	int n;
+	scanf("%d",&n);
+
+	for(int i=0; i<n; i++){
+		scanf("%s",line[i]);
+		gets(other);
+	}
+	qsort(line, n, 75, compare);
+
+	int ans=1;
+	for(int i=0; i<n; i++){
+		if(strcmp(line[i],line[i+1])==0) ans++;
+		else{
+			printf("%s %d\n",line[i],ans);
+			ans=1;
+		}
+	}
+}
