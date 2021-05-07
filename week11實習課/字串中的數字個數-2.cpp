048595f1f3139824fee30907fae4ce79fa1f4d65
lovmin1013 <79676872+lovmin1013@users.noreply.github.com>
2021-05-07 15:32:10 +0800

    Add files via upload

---
+++ week11實習課/字串中的數字個數-2.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    char a[90];
+    scanf("%s",&a);
+    int i=0,ans=0;
+    while(a[i]!='\0'){
+        if(a[i] >= '0' && a[i]<='9') ans++;
+        i++;
+    }
+    printf("%d",ans);
+}
